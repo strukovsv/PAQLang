d4f89b1792d529799cc22f1549f2233552d8bc7d
Sergey Strukov <strukovsv@mai.ru>
2024-03-07 01:45:22 +0700

    test import

--- src/paqlang/__Init__.py
+++ src/paqlang/__Init__.py
@@ -1,5 +1,7 @@
-from .program import pgm
-from .utils import get_json, aio_reads, freads
+from .program import *
+from .utils import *
+# from .program import pgm
+# from .utils import get_json, aio_reads, freads
 
 __all__ = [
     pgm,
