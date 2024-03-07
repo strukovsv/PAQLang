6c2b0ca4b2da5034dd88b7d241e0a0e5ad4ede6f
Sergey Strukov <strukovsv@mail.ru>
2024-03-07 11:10:45 +0700

    Update __Init__.py

--- src/paqlang/__Init__.py
+++ src/paqlang/__Init__.py
@@ -1,7 +1,5 @@
-from .program import *
-from .utils import *
-# from .program import pgm
-# from .utils import get_json, aio_reads, freads
+from .program import pgm
+from .utils import get_json, aio_reads, freads
 
 __all__ = [
     pgm,
